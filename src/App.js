8155439f0c4329b1fdf9060b1b36132cfc5c6592
Illyriat <j4m3s.robson@gmail.com>
2022-10-25 12:39:45 +0100

    second

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [todo, setTodo] = useState([
+    {name: "Buy Shopping", priority: "high", isComplete: true},
+    {name: "Clean Bathroom", priority: "low", isComplete: false},
+    {name: "Car's MOT", priority: "high", isComplete: false}
+  ]);
+
+  const [newTodo, setNewTodo] = useState("")
+  const [priority, setPriority] = useState("")
+
+  const todoNodes = todo.map((todo, index) => {
+    return(
+      <li key={index} className={todo.isComplete ? "complete" : "not-complete"}>
+        <span>{ todo.name }, {todo.priority}: </span>
+        {todo.isComplete ? <span className='complete'>Complete!</span> : <button onClick={() => completeTodo(index)}>Not Complete!</button>}
+      </li>
+    )
+  });
+
+  const handleTodoInput = (event) => {
+    setNewTodo(event.target.value)
+  };
+
+  const handPrioritySelect = (event) => {
+    setPriority(event.target.value)
+  }
+
+  const saveNewTodo = (event) => {
+    event.preventDefault();
+    const copyTodo = [...todo]
+    copyTodo.push({name: newTodo, priority: priority, isComplete: false})
+    setTodo(copyTodo)
+    setNewTodo("")
+    setPriority("")
+  };
+
+
+  const completeTodo = (index) => {
+    const copyTodo = [...todo]
+    copyTodo[index].isComplete = true;
+    setTodo(copyTodo)
+  };
+
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ToDo List</h1>
+
+      <form onSubmit={saveNewTodo}>
+        <label htmlFor='newTodo'>Add an Activity:</label>
+        <input type="text" id="new-todo" onChange={handleTodoInput}/>
+
+          <label htmlFor='high'>High</label>
+          <input type="radio" value="High" id="high" name="prioritySelect" onChange={handPrioritySelect} checked={priority === "high"} />
+
+          <label htmlFor='low'>Low</label>
+          <input type="radio" value="Low" id="low" name="prioritySelect" onChange={handPrioritySelect} checked={priority === "low"}/>
+
+        <input type="submit" value="Save Activity"/>
+
+      </form>
+
+      <ul>
+        {todoNodes}
+      </ul>
+        
     </div>
   );
-}
+};
 
 export default App;
